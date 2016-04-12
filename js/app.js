676a608e15b0ab9601b8b309d0a8a260da79496b
Richard Strutt <richardastrutt@gmail.com>
2016-04-12 23:37:41 +0800

    created new files including css, html, javascript

---
+++ js/app.js
@@ -0,0 +1,31 @@
+var initialMarkers = [
+  {
+    name: 'London Bridge',
+    position: {lat: 51.508245, lng: -0.087700},
+    contentString: 'Short Fact: locacted between the city of London and Southwark'
+  },{
+    name: 'London Eye',
+    position: {lat: 51.502820, lng: -0.119252,
+    contentString: 'Short Fact: its height is 135 meters'
+  },{
+    name: 'St Paul\'s Cathedral',
+    position: {lat: 51.514274, lng: -0.098992,
+    contentString: 'Short Fact: construction started in 1675, opened in 1708'
+  },{
+    name: 'Big Ben',
+    position: {lat: 51.499840, lng: -0.124663,
+    contentString: 'Short Fact: It is of Gothic Revival architecture'
+  },{
+    name: 'Buckingham Palace',
+    position: {lat: 51.501009, lng: -0.141588,
+    contentString: 'Short Fact: Floor space is around 77,000 m²!'
+  }
+];
+
+var Markers = function(){
+
+};
+
+var ViewModel = function(){
+
+};
