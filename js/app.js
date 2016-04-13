7ae799d23b28eead0a4835f4ee61690db8ba3638
Richard Strutt <richardastrutt@gmail.com>
2016-04-13 22:07:23 +0800

    added search & filter KO functionality

--- js/app.js
+++ js/app.js
@@ -1,51 +1,76 @@
-var places = [
+//Defining the collection of places
+var places = ko.observableArray([
   {
     name: 'London Bridge',
     position: {lat: 51.508245, lng: -0.087700},
-    info: "<b>Quick Fact:</b> the bridge is locacted between the city of London and Southwark"
   },{
     name: 'London Eye',
     position: {lat: 51.502820, lng: -0.119252},
-    info: "<b>Quick Fact:</b> its height is 135 meters"
   },{
     name: 'St Paul\'s Cathedral',
     position: {lat: 51.514274, lng: -0.098992},
-    info: "<b>Quick Fact:</b> construction started in 1675, opened in 1708"
   },{
     name: 'Big Ben',
     position: {lat: 51.499840, lng: -0.124663},
-    info: "<b>Quick Fact:</b> It is of Gothic Revival architecture"
   },{
     name: 'Buckingham Palace',
     position: {lat: 51.501009, lng: -0.141588},
-    info: "<b>Quick Fact:</b> Floor space is around 77,000 m²!"
   },{
     name: 'Somerset House',
     position: {lat: 51.510732, lng: -0.116938},
-    info: "<b>Quick Fact:</b> It's a major arts and cultural centre in the heart of London"
   },{
     name: 'Kensington Palace',
     position: {lat: 51.504894, lng: -0.188130},
-    info: "<b>Quick Fact:</b> Kensington Palace is a royal residence set in Kensington Gardens, in the Royal Borough of Kensington and Chelsea in London, England"
   }
-];
+]);
 
-var Markers = function(){
+//Redefine places() into variable
+function ViewModel(){
+    var self = this, 
+        location = places();
+    // Text from search field
+    self.filterText = ko.observable(""); 
+    for (var i = 0; i < location.length; i++) {
+        location[i]["search_content"] = ">";
+        for (var x in location[i] ) {
+            if ( !location[i].hasOwnProperty(x) || x == "search_content" || typeof location[i][x] !== "string") {continue;}
+            location[i]["search_content"] += location[i][x].toUpperCase();
+        }
+    }
+    // Collection of places after going through search filter
+    self.filteredPlaces = ko.computed(function () {
+        var regExp;
+        // If many white spaces in a row, replace with only one white space. (g modifier & case insensitive)
+        fText = self.filterText().replace(/\s+/gi, '|');
+        fText = fText.replace(/\|\s*$/gi, '');
+        //Define a new regular expression
+        regExp = new RegExp(fText, "gi");
+        // If there is anything in the search box, filter for this
+        // As of now this does not divide the filterText and only searches the Name field
+        var filteredCollection = ko.utils.arrayFilter(places(), function(test) {
+            if(fText.length)
+                return test.search_content.match(regExp);
+            else
+                return 1;
+        });
+        return filteredCollection;
+    }, self);  
+}
 
+//Run KO!
+var runDoc = function(){
+    var vm = new ViewModel();
+    ko.applyBindings(vm);
 };
-
-var ViewModel = function(){
-
-};
+runDoc();
 
 
 /*---------GOOGLE MAP------------*/
 
-var map,
+var map, 
     markers = [];
-
 function initMap() {
-
+    var location = places();
     map = new google.maps.Map(document.getElementById('map'), {
         zoom: 12,
         center: new google.maps.LatLng(51.508245, -0.087700),
@@ -54,23 +79,23 @@ function initMap() {
 
     var infowindow = new google.maps.InfoWindow();
 
-    for (var i = 0; i < places.length; i++) {
+    for (var i = 0; i < location.length; i++) {
         setTimeout(function(i) {
           var newMarker = new google.maps.Marker({
-              position: new google.maps.LatLng(places[i].position),
+              position: new google.maps.LatLng(location[i].position),
               map: map,
-              title: places[i].name,
+              title: location[i].name,
               animation: google.maps.Animation.DROP
           });
 
           google.maps.event.addListener(newMarker, 'click', (function (newMarker, i) {
               return function () {
-                  infowindow.setContent(places[i].name);
+                  infowindow.setContent(location[i].name);
                   infowindow.open(map, newMarker);
                   //Bounce the marker
                   newMarker.setAnimation(google.maps.Animation.BOUNCE);
-                  //Then set a timer to stop the marker bouncing after 1450ms (2 bounces)
-                  setTimeout(function(){newMarker.setAnimation(null);}, 1450)
+                  //Then set a timer to stop the marker bouncing after 750ms (1 bounce)
+                  setTimeout(function(){newMarker.setAnimation(null);}, 750)
               }
           })(newMarker, i));
 
