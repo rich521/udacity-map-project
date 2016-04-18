09667f7928be234ec9de8056f09a28bb21101473
Richard Strutt <richardastrutt@gmail.com>
2016-04-18 11:30:02 +0800

    minified files, added error grace functionality, increased loading performance, beautified scripts

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # udacity-map-project
 
+
+### INSTRUCTIONS
+
+1. Open index.html to run the website.
+
+2. Click on the ham-burger menu on the top-left (opens the nav slider).
+
+3. Click on any place, or filter the one you are interested in.
+
+4. *Alternatively*, you can just click on a marker.
+
+5. The marker will display the name of POI or location, an image related to it, and a link of where it came from.
+
+##### Flickr Source
+
+All images are sourced from [Flickr.com](https://www.flickr.com).
+
--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//Hard coded data for example purposes.
+//Hard coded data for marker pin example.
 var initialPlaces = [
   {
     name: 'Big Ben',
@@ -10,22 +10,36 @@ var initialPlaces = [
     name: 'Kensington Palace',
     position: {lat: 51.504894, lng: -0.188130}
   },{
+    name: 'Madame Tussauds London',
+    position: {lat: 51.522965, lng: -0.155051}
+  },{
     name: 'London Bridge',
     position: {lat: 51.508245, lng: -0.087700}
   },{
     name: 'London Eye',
     position: {lat: 51.502820, lng: -0.119252}
   },{
+    name: 'Royal Opera House',
+    position: {lat: 51.513283, lng: -0.123433}
+  },{
+    name: 'Sadler\'s Wells Theatre',
+    position: {lat: 51.529409, lng: -0.106248}
+  },{
     name: 'Somerset House',
     position: {lat: 51.510732, lng: -0.116938}
   },{
     name: 'St Paul\'s Cathedral',
     position: {lat: 51.514274, lng: -0.098992}
+  },{
+    name: 'The British Library',
+    position: {lat: 51.529985, lng: -0.127014}
+  },{
+    name: 'Tower of London',
+    position: {lat: 51.508112, lng: -0.075949}
   }
 ];
-
 /*------------GOOGLE MAP------------*/
-var map, 
+var map,
     markers = [],
     infowindow,
     state = false,
@@ -42,98 +56,115 @@ var initMap = function() {
     var mapPoi = [{
         featureType: "poi",
         stylers: [{
-          visibility: "off"
+            visibility: "off"
         }]
-    },{
-      featureType: "transit",
-      stylers: [
-        { visibility: "off" }
-      ]
+    }, {
+        featureType: "transit",
+        stylers: [{
+            visibility: "off"
+        }]
     }];
 
-    map.setOptions({styles: mapPoi});
+    map.setOptions({
+        styles: mapPoi
+    });
 
     infowindow = new google.maps.InfoWindow();
 
     for (var i = 0; i < initialPlaces.length; i++) {
-        setTimeout(function(i) {
-          var newMarker = new google.maps.Marker({
-              position: new google.maps.LatLng(initialPlaces[i].position),
-              map: map,
-              title: initialPlaces[i].name,
-              animation: google.maps.Animation.DROP,
-              icon: iconBase
-          });
-          google.maps.event.addListener(newMarker, 'click', (function (newMarker, i) {
-              return function () {
-                  //flickrData(initialPlaces[i].name);
-                  openWindow(newMarker, initialPlaces[i].name);
-              }
-          })(newMarker, i));
-          markers.push(newMarker);
-        //200ms interval for each marker created
-        }, i * 200, i);
+        showPin(i);
+    }
+
+    function showPin(i) {
+        setTimeout(function() {
+            var newMarker = new google.maps.Marker({
+                position: new google.maps.LatLng(initialPlaces[i].position),
+                map: map,
+                title: initialPlaces[i].name,
+                animation: google.maps.Animation.DROP,
+                icon: iconBase
+            });
+            google.maps.event.addListener(newMarker, 'click', (function(newMarker, i) {
+                return function() {
+                    //flickrData(initialPlaces[i].name);
+                    openWindow(newMarker, initialPlaces[i].name);
+                };
+            })(newMarker, i));
+            markers.push(newMarker);
+            //120ms interval for each marker created
+        }, i * 0, i);
         //when markers are pushed into array
-        if(i === initialPlaces.length - 1){
-          state = true;
+        if (i === initialPlaces.length - 1) {
+            state = true;
         }
     }
     //Recenter the map when resizing to Latlong 
-    google.maps.event.addDomListenerOnce(map, 'idle', function () {
-      google.maps.event.addDomListener(window, 'resize', function () {
-          map.setCenter(LatLong);
-      });
+    google.maps.event.addDomListenerOnce(map, 'idle', function() {
+        google.maps.event.addDomListener(window, 'resize', function() {
+            map.setCenter(LatLong);
+        });
     });
 };
 
 //InfoWindow function
-var openWindow = function(data, places){
-  var url = 'http://api.flickr.com/services/feeds/photos_public.gne?tags=' + places + '&tagmode=any&format=json&jsoncallback=?';
-  var imgData;
-  var link;
+var openWindow = function(data, places) {
+    var url = 'http://api.flickr.com/services/feeds/photos_public.gne?tags=' + places + '&tagmode=any&format=json&jsoncallback=?';
+    var imgData;
+    var link;
 
-  $.getJSON(url, function(json) {
-    $(json.items).each(function(i, item) {
-      link = item.media.m;
-      imgData = '<div><img src="' + item.media.m + '" alt="Image Source" /></div>';
-        if (i == 0) return false
+    //No error handling available, therefore we set 'setTimeout' if it runs too long
+    var flickrRequestTimeout = setTimeout(function() {
+        infowindow.setContent('<div id="infoWindow"><h4>' + places + '</h4>' + '<h6>Failed to load Flickr image</h6></div>');
+    }, 4000); //4 seconds
+
+    $.ajax({
+        url: url,
+        dataType: 'jsonp',
+        // jsonp: "callback",  --> by default. if needed to be changed
+        success: function(response) {
+            link = response.items[0].media.m;
+            imgData = '<div><img src="' + link + '" alt="Image Source" /></div>';
+            flickr();
+            clearTimeout(flickrRequestTimeout);
+        }
     });
-    flickr(); //Adjust for delay in request
-  });
 
-  infoWindow();
-  function infoWindow(){
-    infowindow.setContent('<div id="infoWindow"><h4>'+places+'</h4>' + '<div id="loading"></div></div>');
-    infowindow.open(map, data);
-    //Bounce the marker
-    data.setAnimation(google.maps.Animation.BOUNCE);
-    //Then set a timer to stop the marker bouncing after 750ms (1 bounce)
-    setTimeout(function(){data.setAnimation(null);}, 750)
-  }
+    infoWindow();
 
-  function flickr(){
-    infowindow.setContent('<div id="infoWindow"><h4>'+ places + '</h4>' + imgData + '<a href="'+ link + '" target="_blank">source: \'Flickr images\'</p></div>');
-  }
+    function infoWindow() {
+        infowindow.setContent('<div id="infoWindow"><h4>' + places + '</h4>' + '<div id="loading"></div></div>');
+        infowindow.open(map, data);
+        //Bounce the marker
+        data.setAnimation(google.maps.Animation.BOUNCE);
+        //Then set a timer to stop the marker bouncing after 750ms (1 bounce)
+        setTimeout(function() {
+            data.setAnimation(null);
+        }, 750);
+    }
+
+    function flickr() {
+        infowindow.setContent('<div id="infoWindow"><h4>' + places + '</h4>' + imgData + '<a href="' + link + '" target="_blank">source: \'Flickr images\'</p></div>');
+    }
 };
 
 //Filter the markers when called
-var filterMarker = function(listed){
-  for (var i = 0; i < listed.length; i++) {    
-    for(var j = 0; j < markers.length; j++) {
-      if(markers[j].title === listed[i].name){
-        markers[j].setVisible(true);
-      }
+var filterMarker = function(listed) {
+    for (var i = 0; i < listed.length; i++) {
+        for (var j = 0; j < markers.length; j++) {
+            if (markers[j].title === listed[i].name) {
+                markers[j].setVisible(true);
+            }
+        }
     }
-  }
 };
 
 /*------------KnockoutJS------------*/
-var Place = function(data){
-  this.name = ko.observable(data.name);
+var Place = function(data) {
+    this.name = ko.observable(data.name);
 };
 
 //Redefine initialPlaces() into variable
-var ViewModel = function(){
+var ViewModel = function() {
     var self = this,
         places = ko.observableArray(initialPlaces),
         location = places(),
@@ -141,53 +172,74 @@ var ViewModel = function(){
 
     //Create an observableArray, each with new Place object
     this.placeList = ko.observableArray([]);
-    initialPlaces.forEach(function(placeItem){
-      self.placeList.push(new Place(placeItem));
-      nameArray.push(placeItem.name)
+    initialPlaces.forEach(function(placeItem) {
+        self.placeList.push(new Place(placeItem));
+        nameArray.push(placeItem.name);
     });
 
     //store our place to a new current place variable
-    this.currentPlace = ko.observable( this.placeList()[0] );
+    this.currentPlace = ko.observable(this.placeList()[0]);
 
     //click to set the selected place
-    this.clickPlace = function(clickedPlace){
-      self.currentPlace(clickedPlace);
-      openWindow(markers[nameArray.indexOf(clickedPlace.name)], clickedPlace.name);
+    this.clickPlace = function(clickedPlace) {
+        self.currentPlace(clickedPlace);
+        openWindow(markers[nameArray.indexOf(clickedPlace.name)], clickedPlace.name);
     };
 
     // Text from search field
-    self.filterText = ko.observable(""); 
+    self.filterText = ko.observable("");
     for (var i = 0; i < location.length; i++) {
-        location[i]["search_content"] = ">";
-        for (var x in location[i]) {
-            if ( !location[i].hasOwnProperty(x) || x == "search_content" || typeof location[i][x] !== "string") {continue;}
-            location[i]["search_content"] += location[i][x].toUpperCase();
+        var content = "search_content",
+            loc = location[i];
+        loc.content = ">";
+        for (var x in loc) {
+            if (loc.hasOwnProperty(x) || x !== content || typeof loc[x] === "string") {
+                loc.content += loc[x];
+            }
         }
     }
     // Collection of initialPlaces after going through search filter
-    self.filteredPlaces = ko.computed(function () {
-        var regExp;
+    self.filteredPlaces = ko.computed(function() {
+        var regExp, fText;
         fText = self.filterText();
         //Define a new regular expression
         regExp = new RegExp(fText, "gi");
         // If there is anything in the search box, filter for this
         var filteredCollection = ko.utils.arrayFilter(location, function(text) {
-            if(fText.length)
-                return text.search_content.match(regExp);
+            if (fText.length)
+                return text.content.match(regExp);
             else
                 return true;
         });
         //Remove all markers first, then show visibile ones matching the search field
-        if(state){
-          for(var z = 0; z < markers.length; z++){
-            infowindow.close();
-            markers[z].setVisible(false);
-          }
-          filterMarker(filteredCollection);
+        if (state) {
+            for (var z = 0; z < markers.length; z++) {
+                infowindow.close();
+                markers[z].setVisible(false);
+            }
+            filterMarker(filteredCollection);
         }
         return filteredCollection;
-    }, self);  
+    }, self);
 
 };
+
+var nav = true;
+
+function openNav() {
+    if (nav) {
+        document.getElementById("mySidenav").style.width = "275px";
+        document.getElementById("main").style.marginLeft = "275px";
+        nav = false;
+    } else {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+        nav = true;
+    }
+    //Wait after transition has ended, then refresh google map to rid grey area bug
+    setTimeout(function() {
+        google.maps.event.trigger(map, 'resize');
+    }, 350);
+}
 //Run the app
 ko.applyBindings(new ViewModel());
